1e3af0f593d014f42397756166ed1548823b6a91
chetan prakash udhane <78649010+chetanudhane@users.noreply.github.com>
2022-09-30 17:29:31 +0530

    UC2 Added

--- StockAccountManagement.java
+++ StockAccountManagement.java
@@ -6,25 +6,35 @@ public class StockAccountManagement {
 
 	public static void main(String[] args) {
 
-		System.out.println("Stock Account Management Program ! \n");
+		System.out.println("Stock Account Management Program !");
 		@SuppressWarnings("resource")
 		Scanner sc = new Scanner(System.in);
 		StockManagement stockManagement = new StockManagement();
+		AccountTest accountTest = new AccountTest();
+
 		boolean isExit = true;
 		do {
-			System.out.println("Select the option from below : " + "\n1.Add Stock" + "\n2.print stock report"
-					+ "\n3.Exit program");
+			System.out.println("Select the option from below : " + "\n1.Add Stock" + "\n2.Print Stock Report"
+					+ "\n3.Credit Amount" + "\n4.Debit Amount" + "\n5.Check Balance" + "\n6.Exit program");
 			int choice = sc.nextInt();
 
 			switch (choice) {
 			case 1:
 				stockManagement.addStock();
 				break;
-
 			case 2:
 				stockManagement.printStock();
 				break;
 			case 3:
+				accountTest.credit();
+				break;
+			case 4:
+				accountTest.debit();
+				break;
+			case 5:
+				accountTest.check_Balance();
+				break;
+			case 6:
 				isExit = false;
 				System.out.println("Exit");
 				break;
